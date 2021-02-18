8a8809532f035aabc53671809e8be234230e7e75
wnwlals13 <wnwlals13@naver.com>
2021-02-18 16:06:18 +0900

    update secret info

--- index.js
+++ index.js
@@ -1,24 +1,40 @@
 //백엔드 시작점
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 5000;
+
+const config = require("./config/dev");
+
+const { User } = require("./models/User");
+
+const bodyParser = require("body-parser");
+//application/x-ww-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+//application/json
+app.use(bodyParser.json());
 
 const mongoose = require("mongoose");
 mongoose
-  .connect(
-    "mongodb+srv://jimin:jimin1234@boilerplate.ymnqg.mongodb.net/boilerplate?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    }
-  )
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
-  res.send("Hello World!~~~");
+  res.send("Hello World!~~~!!!!");
+});
+
+app.post("/register", (req, res) => {
+  // 회원가입 정보를 client에서 가져와 데이터베이스에 넣는다.
+  const user = new User(req.body);
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({ success: true });
+  });
 });
 
 app.listen(port, () => {
